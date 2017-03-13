c687565b995ae4799139c6cf26020e16a9275c6f
Thomas Ollivier <thomasollivierfrance@gmail.com>
2017-03-13 18:07:56 +0100

    seed v1

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,9 @@
 
 # user
 10.times do
-  # name = Faker::Name.name
-  # age = [18..45].to_a.sample
   email = Faker::Internet.email
   password = 123456
   User.create(email: email, password: password)
-  # name: name, age: age,
 end
 
 # personnality
@@ -35,5 +32,18 @@ end
   end_date = Faker::Date.forward(60)
   status = "pending"
   Rental.create(user: user, personnality: personnality, start_date: start_date, end_date: end_date, status: status)
+end
+
+User.all.each do |user|
+  user.name = Faker::Name.name
+  user.age = [18..45].to_a.sample
+  user.save!
+end
+
+Personnality.all.each do |p|
+  user = User.find(p.user)
+  p.name = user.name
+  p.age = user.age
+  p.save!
 end
 
