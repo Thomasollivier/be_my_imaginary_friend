3a5c685175abbe3386d5bec0332c8ade2f035218
Thomas Ollivier <thomasollivierfrance@gmail.com>
2017-03-13 17:23:46 +0100

    création des routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :personnalities, only: [:new, :create, :show, :index] do
+    resources :rentals, only: [:create]
+  end
+  resources :reservations, only: [:show] do
+    member do
+      patch :accept
+      patch :refuse
+    end
+  end
+  resources :rentals, only: [:index]
 end
