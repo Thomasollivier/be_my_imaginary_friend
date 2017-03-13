3225ab2714c6c9cdb93a77341efbbe2f60e8b39d
Thomas Ollivier <thomasollivierfrance@gmail.com>
2017-03-13 17:54:15 +0100

    seeds v0

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# user
+10.times do
+  # name = Faker::Name.name
+  # age = [18..45].to_a.sample
+  email = Faker::Internet.email
+  password = 123456
+  User.create(email: email, password: password)
+  # name: name, age: age,
+end
+
+# personnality
+10.times do
+  job = Faker::Company.profession
+  friends = Faker::Pokemon.name
+  main_emotion = %w(fear anger sadness depression joy disgust trust anticipation pity envy love shame kindness suffering weeping lust gluttony greed sloth wrath pride).sample
+  user = User.all.sample
+  name = Faker::Name.name
+  age = [18..45].to_a.sample
+  Personnality.create(name: name, age: age, job: job, friends: friends, main_emotion: main_emotion, user: user)
+end
+
+# rental
+20.times do
+  user = User.all.sample
+  personnality = Personnality.all.sample
+  start_date = Date.today
+  end_date = Faker::Date.forward(60)
+  status = "pending"
+  Rental.create(user: user, personnality: personnality, start_date: start_date, end_date: end_date, status: status)
+end
+
